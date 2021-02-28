b9829f28847ff215c4ba393eac4cd4fb3a62cb52
Andrea9618 <aguzmang1@est.ups.edu.ec>
2021-02-28 11:51:04 -0500

    Se realizo algunos cambios

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.pjyad.gcp.mongodb.net/utups?retryWrites=true&w=majority',
-    port: process.env.PORT || 5000,
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups_mongodb:Aki9618Misaclusteru3taller02.qnmsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    port: process.env.PORT || 3306,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
     filesRoute: process.env.FILES_ROUTE || 'files',
