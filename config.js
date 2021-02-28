f3106ef943773f56da60257046f60e8d1f8e35e4
Andrea9618 <aguzmang1@est.ups.edu.ec>
2021-02-28 18:31:51 -0500

    version 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups_mongodb:Aki9618Misaclusteru3taller02.qnmsi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups_mongodb:Aki9618Misa@clusteru3taller02.qnmsi.mongodb.net/utups?retryWrites=true&w=majority',
     port: process.env.PORT || 3306,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
