50026fe2cccee4f1796d98a2c34931a0bfa53b6c
Andrea9618 <aguzmang1@est.ups.edu.ec>
2021-02-28 21:03:45 -0500

    version 2

--- components/titulacion/storage.js
+++ components/titulacion/storage.js
@@ -1,12 +1,6 @@
 const model = require('./model')
 
-function getTitulacion(filtroTitulacion) {
-    let filtro = {}
-    if (filtroTitulacion != null) {
-        filtro = { nombre : filtroTitulacion }
-    }
-    const titulacionList = await model.find( filtro )
-    return titulacionList
+function getTitulacion() {
 
 }
 
@@ -15,23 +9,12 @@ function addTitulacion( objeto ) {
     titulacion.save()
 }
 
-async function updateTitulacion( idTitulacion, objeto) {
-    const foundTitulacion = await model.findOne({ _id: idTitulacion })
+function updateTitulacion() {
 
-    foundTitulacion.carrera = objeto.carrera
-    foundTitulacion.tutor = objeto.tutor
-    foundTitulacion.revisor = objeto.revisor
-    foundTitulacion.tipo_titulacion = objeto.tipo_titulacion
-    foundTitulacion.archivo = objeto.archivo
-    foundTitulacion.fecha_archivo = objeto.fecha_archivo
-    foundTitulacion.estado = objeto.estado
-
-    const result = await foundTitulacion.save()
-    return result
 }
 
-function deleteTitulacion(idCarrera) {
-    return model.deleteOne({ _id: idCarrera })
+function deleteTitulacion() {
+
 }
 
 module.exports = {
